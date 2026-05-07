26bd332256e49f7292f9ab27fc1801db7083a9cb
jzarcoo <zarco_romero_jose_antonio@comunidad.unam.mx>
2026-05-06 20:41:48 -0600

    fix: manacher algorithm

--- String Algorithms/Longest Palindrome.cpp
+++ String Algorithms/Longest Palindrome.cpp
@@ -1,32 +1,45 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-  ios::sync_with_stdio(0); cin.tie(0);
-  string s; cin >> s;
-  int n = s.size();
-  string t; t.reserve(2 * n + 1);
-  for (char c : s) {
-	t.push_back('#');
-	t.push_back(c);
-  }
-  t.push_back('#');
-  int m = t.size();
-  vector<int> p(m);
-  int l = 0, r = -1, best_len = 0, best_center = 0;
-  for (int i = 0; i < m; i++) {
-	int &k = p[i];
-    k = (i > r) ? 1 : min(p[l + r - i], r - i + 1);
-	while (i - k >= 0 && i + k < m && t[i - k] == t[i + k])
-	  k++;
-	if (i + k - 1 > r) {
-	  l = i - k + 1;
-	  r = i + k - 1;
-	}
-	if (k - 1 > best_len) {
-	  best_len = k - 1;
-	  best_center = i;
-	}
-  }
-  cout << s.substr((best_center - best_len) / 2, best_len);
-  return 0;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    string s; cin>>s;
+    int n = s.length();
+
+    //manacher
+    string t;
+    for(char c: s){
+        t += string("#")+c;
+        
+    }
+    t+="#";
+
+    int nt = t.size();
+    t = "$" + t + "^";
+    
+    vector<int> p(nt +2);
+    int l=0, r=1;
+    for(int i=1; i<=nt; i++){
+        if(i<=r){
+            p[i] = min(r-i, p[l + r-i]);
+        }
+        while(t[i-p[i]] == t[i+p[i]]){
+            p[i]++;
+        }
+        if(i+p[i] > r){
+            l = i - p[i];
+            r = i + p[i];
+        }
+    }
+
+    int center = 0, len = 0;
+    for(int i=1; i<=nt; i++){
+        if(p[i] > len){
+            len = p[i];
+            center = i;
+        }
+    }
+
+    len--;
+    int ini = (center-1-len)/2;
+    cout << s.substr(ini, len)<<endl;
 }
