5e8200366faf608f10499e07a997f1862c5dcf3f
jzarcoo <zarco_romero_jose_antonio@comunidad.unam.mx>
2026-08-28 10:12:17 -0600

    segment tree + pbds + coordinate compression

--- Sliding Window Problems/Sliding Window Cost.cpp
+++ Sliding Window Problems/Sliding Window Cost.cpp
@@ -1,45 +1,121 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
 using namespace __gnu_pbds;
+
+typedef pair<int,int> ii;
 typedef long long ll;
-typedef pair<int, int> p;
+
 typedef tree<
-  p,
-  null_type,
-  less<p>,
-  rb_tree_tag,
-  tree_order_statistics_node_update
-  > indexed_set;
-/*
-  Input:
+ii, 
+null_type,
+less<ii>,
+rb_tree_tag,
+tree_order_statistics_node_update
+> o_set;
 
-8 3
-2 4 3 5 8 1 2 1
+#define mid(l,r) (l)  + ((r)-(l)) / 2
 
-  Output:
+const int N = 2e5+5;
 
-  2 2 5 7 7 1
-
-*/
-indexed_set s;
 int n, k;
-ll sum = 0;
+int in[N];
+
+int med;
+
+ll st[4*N];
+o_set t;
+
+unordered_map<int, int> original, compress;
+int distinct;
+
+void compression(){
+    vector<int> vals(in+1, in+n+1);
+    sort(vals.begin(), vals.end());
+    vals.erase(unique(vals.begin(), vals.end()), vals.end());
+    
+    for(int i=1; i<=n; i++){
+        int value = in[i];
+        int idx = lower_bound(vals.begin(), vals.end(), value) - vals.begin() + 1;
+        compress[value] = idx;
+        original[idx] = value;
+    }
+
+    distinct = vals.size();
+}
+
+void update(int v, int tl, int tr, int pos, ll d){
+    if(tl==tr){
+        st[v] += d;
+        return;
+    }
+    int tm = mid(tl,tr);
+    if(pos<=tm){
+        update(2*v, tl, tm, pos, d);
+    }else{
+        update(2*v+1, tm+1, tr, pos, d);
+    }
+    st[v] = st[2*v] + st[2*v+1];
+}
+
+ll query(int v, int tl, int tr, int ql, int qr){
+    if(tl > qr || tr < ql){
+        return 0;
+    }
+    if(ql <= tl && tr <= qr){
+        return st[v];
+    }
+    int tm = mid(tl, tr);
+    return query(2*v, tl, tm, ql, qr) + query(2*v+1, tm+1, tr, ql, qr);
+}
+
+ll ans_window(){
+    ll ans;
+
+    auto it = t.find_by_order(med);
+    auto [median, median_i] = *it;
+  
+    // number of 
+    int lower_values = t.order_of_key({median, -1});
+    int upper_values = t.order_of_key({median, N});
+
+    ll q = query(1, 1, distinct, 1, compress[median]);
+    //cout <<"q: "<<q<<endl;
+    ll to_increment = 1LL * lower_values * median - q;
+    ll to_decrement = - 1LL * (k-upper_values) * median + query(1, 1, distinct, compress[median], distinct);
+    
+    ans = to_increment + to_decrement;
+
+    return ans;
+}
+
+void solve(){
+    compression();
+    
+    med = (k-1)/2;
+
+    for(int i=1; i<=n; i++){
+        if (i > k) {
+            int j = i-k;
+            t.erase({in[j], j});
+            update(1, 1, distinct, compress[in[j]], -in[j]);
+        }
+        t.insert({in[i], i});
+        update(1, 1, distinct, compress[in[i]], in[i]);
+        if (i >= k) {
+            cout << ans_window() << ' ';
+        }
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin>>n>>k;
-    vector<int> v(n); for(auto &i: v)cin>>i;
-    for(int i=0; i<n; i++){
-        sum += v[i];
-        s.insert({v[i],i});
-        if(i-k+1>=0){
-            auto it = s.find_by_order((k-1) /2);
-            p ans  = *it;
-            cout << abs(k*(ans.first))  << ' '<<sum<<endl;
-            s.erase({v[i-k+1], i-k+1});
-            sum -= v[i-k+1];
-        }
+    for(int i=1; i<=n; i++){
+        cin>>in[i];
     }
-    return 0;
+    solve();
 }
